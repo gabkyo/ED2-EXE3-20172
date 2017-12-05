32c3ae0e30cd2de31573154f53e68b27ee7f9985
gabkyo <34278677+gabkyo@users.noreply.github.com>
2017-12-05 15:13:23 -0200

    Add files via upload

--- 5.43-extraipalavra.h
+++ 5.43-extraipalavra.h
@@ -1,9 +1,5 @@
-/* Informar nomes dos arquivos teste.txt e alfabeto.txt na linha de comando */
-/* Exemplo: a.out teste.txt alfabeto.txt                                    */
-
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef EXTRAI_H_
+#define EXTRAI_H
 #define MAXALFABETO 255
 #define TRUE  1
 #define FALSE 0
@@ -16,44 +12,12 @@ char Linha[256];
 int i;
 short aux;
 
-void DefineAlfabeto(short *Alfabeto) {
-	char Simbolos[MAXALFABETO + 1];
-	int i, CompSimbolos;
-	char *TEMP;
-	for (i = 0; i <= MAXALFABETO; i++)
-		Alfabeto[i] = FALSE;
-	fgets(Simbolos, MAXALFABETO + 1, ArqAlf);
-	TEMP = strchr(Simbolos, '\n');
-	if (TEMP != NULL)
-		*TEMP = 0;
-	CompSimbolos = strlen(Simbolos);
-	for (i = 0; i < CompSimbolos; i++)
-		Alfabeto[Simbolos[i] + 127] = TRUE;
-	Alfabeto[0] = FALSE; /* caractere de codigo zero: separador */
-}
+typedef struct Lista_de_Palavras {
+	int tamanho;
+	char **palavras;
+} Lista_de_Palavras;
 
-void ler_arquivo(char *texto, char *alfabeto) {
-	ArqTxt = fopen(texto, "r");
-	ArqAlf = fopen(alfabeto, "r");
-	DefineAlfabeto(Alfabeto); /* Le alfabeto definido em arquivo */
-	aux = FALSE;
-	while (fgets(Linha, 256, ArqTxt) != NULL) {
-		for (i = 1; i <= strlen(Linha); i++) {
-			if (Alfabeto[Linha[i - 1] + 127]) {
-				sprintf(Palavra + strlen(Palavra), "%c", Linha[i - 1]);
-				aux = TRUE;
-			} else if (aux) {
-				puts(Palavra); //palavra lida
-				*Palavra = '\0';
-				aux = FALSE;
-			}
-		}
-	}
-	if (aux) {
-		puts(Palavra); //palavra lida
-		*Palavra = '\0';
-	}
-	fclose(ArqTxt);
-	fclose(ArqAlf);
-}
-/* End. */
+void DefineAlfabeto(short *Alfabeto);
+
+Lista_de_Palavras *ler_arquivo(char *texto, char *alfabeto);
+#endif
---
+++ main.c
@@ -0,0 +1,24 @@
+#include "5.28a5.29-hash-endaberto.c"
+
+int main(int argc, char *argv[]) {
+	arq = NULL;
+	int i = 0;
+	Inicializa(Tabela);
+
+	Lista_de_Palavras *lista = ler_arquivo(argv[1], argv[2]);
+	GeraPesos(p);
+	//LerPalavra(lista->palavras[0], N);
+	strcpy(Elemento.Chave, lista->palavras[0]);
+	while (strcmp(Elemento.Chave, "aaaaaaaaaa") != 0 && i < lista->tamanho) {
+		Insere(Elemento, p, Tabela);
+		strcpy(Elemento.Chave, lista->palavras[i]);
+		i++;
+	}
+
+	printf("Tabela apos insercao:\n");
+	Imprime(Tabela);
+
+	if (arq != NULL)
+		fclose(arq);
+	return 0;
+}
