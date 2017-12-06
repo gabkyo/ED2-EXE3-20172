1a529bd2d5ab635b5110a3c8f77b7a9599cd6642
gabkyo <34278677+gabkyo@users.noreply.github.com>
2017-12-06 13:27:52 -0200

    Add files via upload

--- 5.28a5.29-hash-endaberto.h
+++ 5.28a5.29-hash-endaberto.h
@@ -1,5 +1,8 @@
-#ifndef HASH_H_
-#define HASH_H_
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "5.43-extraipalavra.h"
 #define VAZIO  "!!!!!!!!!!"
 #define RETIRADO  "**********"
 
@@ -26,19 +29,94 @@ TipoItem Elemento;
 FILE *arq;
 int j, i;
 
-void GeraPesos(TipoPesos p);
+void GeraPesos(TipoPesos p) { /* -Gera valores randomicos entre 1 e 10.000- */
+	int i;
+	struct timeval semente;
+	/* Utilizar o tempo como semente para a funcao srand() */
+	gettimeofday(&semente, NULL);
+	srand((int) (semente.tv_sec + 1000000 * semente.tv_usec));
+	for (i = 0; i < N; i++)
+		p[i] = 1 + (int) (10000.0 * rand() / (RAND_MAX + 1.0));
+}
 
-TipoIndice h(TipoChave Chave, TipoPesos p);
-void Inicializa(TipoDicionario T);
+TipoIndice h(TipoChave Chave, TipoPesos p) {
+	int i;
+	unsigned int Soma = 0;
+	int comp = strlen(Chave);
+	for (i = 0; i < comp; i++)
+		Soma += (unsigned int) Chave[i] * p[i];
+	return (Soma % M);
+}
 
-TipoApontador Pesquisa(TipoChave Ch, TipoPesos p, TipoDicionario T);
+void Inicializa(TipoDicionario T) {
+	int i;
+	for (i = 0; i < M; i++)
+		memcpy(T[i].Chave, VAZIO, N);
+}
 
-void Insere(TipoItem x, TipoPesos p, TipoDicionario T);
+TipoApontador Pesquisa(TipoChave Ch, TipoPesos p, TipoDicionario T) {
+	unsigned int i = 0;
+	unsigned int Inicial;
+	Inicial = h(Ch, p);
+	while (strcmp(T[(Inicial + i) % M].Chave, VAZIO) != 0
+			&& strcmp(T[(Inicial + i) % M].Chave, Ch) != 0 && i < M)
+		i++;
+	if (strcmp(T[(Inicial + i) % M].Chave, Ch) == 0)
+		return ((Inicial + i) % M);
+	else
+		return M; /* Pesquisa sem sucesso */
+}
 
-void Retira(TipoChave Ch, TipoPesos p, TipoDicionario T);
+void Insere(TipoItem x, TipoPesos p, TipoDicionario T) {
+	unsigned int i = 0;
+	unsigned int Inicial;
+	if (Pesquisa(x.Chave, p, T) < M) {
+		printf("Elemento ja esta presente\n");
+		return;
+	}
+	Inicial = h(x.Chave, p);
+	while (strcmp(T[(Inicial + i) % M].Chave, VAZIO) != 0
+			&& strcmp(T[(Inicial + i) % M].Chave, RETIRADO) != 0 && i < M)
+		i++;
+	if (i < M) {
+		strcpy(T[(Inicial + i) % M].Chave, x.Chave);
+		/* Copiar os demais campos de x, se existirem */
+	} else
+		printf(" Tabela cheia\n");
+}
 
-void Imprime(TipoDicionario tabela);
+void Retira(TipoChave Ch, TipoPesos p, TipoDicionario T) {
+	TipoIndice i;
+	i = Pesquisa(Ch, p, T);
+	if (i < M)
+		memcpy(T[i].Chave, RETIRADO, N);
+	else
+		printf("Registro nao esta presente\n");
+}
 
-void LerPalavra(char *p, int Tam);
+void Imprime(TipoDicionario tabela) {
+	int i, j, tam;
+	for (i = 0; i < M; i++) {
+		printf("%d  ", i);
+		tam = strlen(tabela[i].Chave);
+		for (j = 0; j < tam; j++)
+			putchar(tabela[i].Chave[j]);
+		putchar('\n');
+	}
+} /* Imprime */
 
-#endif
+void LerPalavra(char *p, int Tam) {
+	char c;
+	int i, j;
+	fflush(stdin);
+	j = 0;
+	while (((c = getchar()) != '\n') && (j < (Tam - 1)))
+		p[j++] = c;
+	p[j] = '\0';
+	while (c != '\n')
+		c = getchar();
+	/* Desconsiderar espacos ao final da cadeia como ocorre no Pascal.*/
+	for (i = j - 1; (i >= 0 && p[i] == ' '); i--)
+		p[i] = '\0';
+}
+
--- 5.43-extraipalavra.h
+++ 5.43-extraipalavra.h
@@ -1,5 +1,11 @@
-#ifndef EXTRAI_H_
-#define EXTRAI_H
+/* Informar nomes dos arquivos teste.txt e alfabeto.txt na linha de comando */
+/* Exemplo: a.out teste.txt alfabeto.txt                                    */
+
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 #define MAXALFABETO 255
 #define TRUE  1
 #define FALSE 0
@@ -17,7 +23,60 @@ typedef struct Lista_de_Palavras {
 	char **palavras;
 } Lista_de_Palavras;
 
-void DefineAlfabeto(short *Alfabeto);
+void DefineAlfabeto(short *Alfabeto) {
+	char Simbolos[MAXALFABETO + 1];
+	int i, CompSimbolos;
+	char *TEMP;
+	for (i = 0; i <= MAXALFABETO; i++)
+		Alfabeto[i] = FALSE;
+	fgets(Simbolos, MAXALFABETO + 1, ArqAlf);
+	TEMP = strchr(Simbolos, '\n');
+	if (TEMP != NULL)
+		*TEMP = 0;
+	CompSimbolos = strlen(Simbolos);
+	for (i = 0; i < CompSimbolos; i++)
+		Alfabeto[Simbolos[i] + 127] = TRUE;
+	Alfabeto[0] = FALSE; /* caractere de codigo zero: separador */
+}
 
-Lista_de_Palavras *ler_arquivo(char *texto, char *alfabeto);
-#endif
+Lista_de_Palavras *ler_arquivo(char *texto, char *alfabeto) {
+	ArqTxt = fopen(texto, "r");
+	ArqAlf = fopen(alfabeto, "r");
+	Lista_de_Palavras *lista;
+	lista = malloc(sizeof(Lista_de_Palavras));
+	lista->tamanho = 0;
+	DefineAlfabeto(Alfabeto); /* Le alfabeto definido em arquivo */
+	aux = FALSE;
+	while (fgets(Linha, 256, ArqTxt) != NULL) {
+		for (i = 1; i <= strlen(Linha); i++) {
+			if (Alfabeto[Linha[i - 1] + 127]) {
+				sprintf(Palavra + strlen(Palavra), "%c", Linha[i - 1]);
+				aux = TRUE;
+			} else if (aux) {
+				lista->palavras =realloc(lista->palavras,sizeof(char) * 256 * (lista->tamanho + 1));
+				strcpy(lista->palavras[lista->tamanho], Palavra);
+				++(lista->tamanho);
+				puts(Palavra); //palavra lida
+				*Palavra = '\0';
+				aux = FALSE;
+			}
+		}
+	}
+	if (aux) {
+		lista->palavras = realloc(lista->palavras,sizeof(char) * 256 * (lista->tamanho + 1));
+		strcpy(lista->palavras[lista->tamanho], Palavra);
+		++(lista->tamanho);
+		puts(Palavra); //palavra lida
+		*Palavra = '\0';
+	}
+	for (int i = 0; i < lista->tamanho; ++i) {
+		for (int j = 0; j < strlen(lista->palavras[i]); ++j) {
+			lista->palavras[i][j] = tolower(lista->palavras[i][j]); //tudo em lowercase
+		}
+	}
+	fclose(ArqTxt);
+	fclose(ArqAlf);
+	return lista;
+}
+
+/* End. */
